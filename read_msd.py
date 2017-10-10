68d8c246d5e0b0f543c40571895ed6167a740935
nickmanginas <nickmanginas@gmail.com>
2017-10-10 16:31:16 +0100

    From test

--- read_msd.py
+++ read_msd.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 
+''' ---------------------------------- Version 0.1 --------------------------'''
+
 style.use('dark_background')
 
 def read_file(path):
