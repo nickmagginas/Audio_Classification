3c1590bdc01b40211aa5370406ee0b2c8648246d
nickmanginas <nickmanginas@gmail.com>
2017-10-10 19:08:16 +0100

    Module Complete

--- filters.py
+++ filters.py
@@ -3,6 +3,8 @@ import soundfile as sf
 from scipy.fftpack import fft
 import matplotlib.pyplot as plt
 from matplotlib import style
+from scipy.integrate import simps
+from numpy import trapz
 
 style.use('dark_background')
 
@@ -35,22 +37,42 @@ def filter(data , cutoff = None , cut_low = None):
 	new_data = {'Amplitude' : [] , 'Frequency' : []}
 	for i in range(0 , len(data['Frequency'])):
 		if data['Frequency'][i] < cutoff and data['Frequency'][i] > cut_low :
-			print(data['Frequency'][i])
 			new_data['Amplitude'].append(data['Amplitude'][i])
 			new_data['Frequency'].append(data['Frequency'][i])
 
 	return new_data
 
+def calculate_area(data , Simpsons = True): 
+	if Simpsons: 
+		try : 
+			simps(data['Amplitude'] , dx = (data['Frequency'][1] - data['Frequency'][0]))
+		except :  
+			print('Error : Cannot apply Simpsons Rule')
+		return simps(data['Amplitude'] , dx = (data['Frequency'][1] - data['Frequency'][0]))
+	return trapz(data['Amplitude'] , dx = (data['Frequency'][1] - data['Frequency'][0]))
+
+def discrete_spectral_density(data , spacing = 50):
+	areas = []
+	for i in range(0 , int(np.max(data['Frequency'])) , spacing):
+		areas.append(calculate_area(filter(data , i + spacing , i) , Simpsons = True))
+	return areas
+
+def plot(y , data):
+	x = list(range(0 , int(np.max(data['Frequency'])) , 10))
+	plt.plot(x , y)
+	plt.show()
+
 
 
 
 def main(): 
-	filename = 'C:\\Users\\nick\\Desktop\\Dissertation\\Genres\\classical\\classical.00019.au'
+	filename = 'C:\\Users\\nick\\Desktop\\Dissertation\\Genres\\jazz\\jazz.00024.au'
 	data , fs = audioread(filename)
 	data = furrier_transform(data , fs)
-	data = filter(data , 100 , 50)
-	plt.plot(data['Frequency'] , data['Amplitude'])
-	plt.show()
+	spectral_info = discrete_spectral_density(data , spacing = 10)
+	plot(spectral_info , data)
+	
+
 
 
 
