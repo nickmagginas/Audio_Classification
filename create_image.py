b1befbdb6cf6a573b56b888419e7a2c66bf23934
nickmanginas <nickmanginas@gmail.com>
2017-10-16 19:08:12 +0100

    Minor Adittions

--- create_image.py
+++ create_image.py
@@ -26,13 +26,33 @@ def frequency_concistency_test(segments_transformed):
 			return
 	print('Dimension test passsed')
 
-def plot_data(segments_transformed): 
-	for i in range(0 , len(segments_transformed)): 
-		x = segments_transformed[i]['Frequency']
-		y = segments_transformed[i]['Amplitude']
-		plt.plot(x , y)
+def plot_data(segments_transformed , stacked = True): 
+
+	if not stacked :
+		for i in range(0 , len(segments_transformed)): 
+			x = segments_transformed[i]['Frequency']
+			y = segments_transformed[i]['Amplitude']
+			plt.plot(x , y)
+			plt.show()
+			return
+
+	dicplot={}
+	fig = plt.figure()
+	for i in range(1 , 29):
+
+		dicplot[str(i)] = fig.add_subplot(28,1,i)
+		dicplot[str(i)].plot(segments_transformed[i-1]['Frequency'] , segments_transformed[i-1]['Amplitude'])
 
 	plt.show()
+
+def create_image(data): 
+	data = [data[i]['Amplitude'] for i in range(0 , len(data))]
+	test_data = data[0]
+	for i in range(1 , len(data)):
+		test_data.extend(data[i])
+	maximum = np.max(test_data)
+	for i in range(0 , len(data)):
+		test_data.extend(data[i])
 
 
 
@@ -40,12 +60,15 @@ def main():
 	filename = 'C:\\Users\\nick\\Desktop\\Dissertation\\Genres\\jazz\\jazz.00012.au'
 	data , fs = audioread(filename)
 	segments = create_segments(data , fs)
-	segments_transformed = [filter(furrier_transform(segments[i],fs) , 2000 , 0) for i in range(0 , 28)]
-	frequency_concistency_test(segments_transformed)
 
+	segments_transformed = [filter(furrier_transform(segments[i],fs) , 2500 , 0) for i in range(0 , 28)]
+	frequency_concistency_test(segments_transformed)
+	#create_image(segments_transformed)
 	plot_data(segments_transformed)
 
 
 
 if __name__ == '__main__': 
 	main()
+
+
